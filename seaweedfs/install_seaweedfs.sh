77f3774329d935a91bbc6f0d882740cb87f2aa1f
zbalint <balint.zoltan.marton@gmail.com>
2024-11-29 22:00:39 +0100

    wip

---
+++ seaweedfs/install_seaweedfs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+readonly SEAWEEDFS_ARCHIVE_URL="https://github.com/seaweedfs/seaweedfs/releases/download/3.80/linux_amd64.tar.gz"
+readonly SEAWEEDFS_BIN_PATH="/usr/sbin/weed"
---
+++ seaweedfs/systemd/wait_for_mount.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for ((i = 0 ; i < 30 ; i++ )); do 
+    if mountpoint -q /opt/docker/volumes/*/; then
+        echo "SeaweedFs volume mounted after ${i}s."
+        echo "Waiting an additional 5s just in case before exiting."
+        sleep 5
+        systemctl start docker.socket
+        systemctl start docker.service
+        break
+    else
+        echo "SeaweedFs volume not mounted! Waiting ${i}s before continue...."
+        sleep $i
+    fi
+done
