a06d0c2c49d8f7417fe016915182a4c24604e928
zbalint <balint.zoltan.marton@gmail.com>
2024-12-09 23:18:52 +0100

    wip

--- seaweedfs/systemd/wait_for_weed_mount.sh
+++ seaweedfs/systemd/wait_for_weed_mount.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for ((i = 0 ; i < 30 ; i++ )); do 
-    if mountpoint -q /opt/docker/volumes/*/; then
-        echo "SeaweedFs volume mounted after ${i}s."
+    if mountpoint -q /opt/docker/volumes; then
+        echo "SeaweedFs volume mounted at /opt/docker/volumes after ${i}s."
         echo "Waiting an additional 5s just in case before exiting."
         sleep 5
         systemctl start docker.socket
